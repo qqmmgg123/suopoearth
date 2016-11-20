e56cdd39c66faa59899993ef4c0cfbc0c31d120e
qqmmgg123 <qqmmgg123@gmail.com>
2016-11-20 18:11:48 +0800

    New.

--- public/js/common.js
+++ public/js/common.js
@@ -310,13 +310,13 @@ var common = {
         };
     },
     statistics: function() {
-        /*var _hmt = _hmt || [];
+        var _hmt = _hmt || [];
         (function() {
             var hm = document.createElement("script");
             hm.src = "//hm.baidu.com/hm.js?9ef942b0d6b160b80ac87ad7fdbb7d5f";
             var s = document.getElementsByTagName("script")[0]; 
             s.parentNode.insertBefore(hm, s);
-        })();*/
+        })();
     }
 };
 
